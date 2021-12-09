0673720bc04bcfe476a196bde7c70a12e6572fbc
Kumar Abhishek <76611663+abhicoderss@users.noreply.github.com>
2021-12-09 13:55:10 +0530

    First Code in Java

---
+++ helloworld.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World ");
+        String name = "My Name is Kumar Abhishek";
+        int age = 20;
+
+        String bestfriend = "Kaif";
+        String girlfriend = " Trisha";
+
+        System.out.println(name);
+        System.out.print("My girlfriend name is" );
+        System.out.println(girlfriend);
+        System.out.print("Her age is " );
+        System.out.println(age);
+        System.out.print("My Bestfriend name is " );
+        System.out.println(bestfriend);
+
+
+    }
+}
